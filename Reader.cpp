560721572b3d88cb5a398c4bc53d8dbaccf9090d
ashish <ashish@localhost.localdomain>
2016-12-12 21:28:48 -0800

    Add buffer logic and thread vector

--- Reader.cpp
+++ Reader.cpp
@@ -1,7 +1,7 @@
 #include "Reader.h"
-#include <iostream>
 #include <fstream>
 #include <string>
+
 
 Reader::Reader()
 {
@@ -15,21 +15,46 @@ bool Reader::parseFile(const char *fileName, int numThreads)
 {
     mFileName = fileName;
     mNumThreads = numThreads;
-    std::string lineBuffer;
+    std::string line;
+    Buffer buffer[mNumThreads];    
+    int currBuffIndex = 0;
 
     if (mNumThreads<1)
     {
         std::cout<<"Needs more than 1 thread for parsing"<<std::endl;
         return false;
+    }    
+
+
+    // Create threads
+    for (int thId = 0; thId < mNumThreads; ++thId)
+    {
+        mThreads.push_back(std::thread(&Reader::_processLines, this));
     }
 
     std::ifstream inptFile(mFileName);
     if (inptFile.is_open() )
     {
-        while ( getline(inptFile, lineBuffer) )
+        while ( getline(inptFile, line) )
         {
-            std::cout<<lineBuffer<<std::endl;
-        }
+            buffer[currBuffIndex].lines.push_back( line );
+
+	    // Change buffer if needed and mark current buffer as ready.
+	    if ( 5 == currBuffIndex )
+	    {
+	        int temp = currBuffIndex;
+	        currBuffIndex = (currBuffIndex + 1) % mNumThreads; 
+	        buffer[temp].isReady = true;
+	    }
+	}
+	// Check if current buffer has some remaining lines, if so, then
+	// process them here.
+	while ( buffer[currBuffIndex].lines.size() )
+	{
+	    std::vector<std::string> l = buffer[currBuffIndex].lines;
+            std::cout<<l.front()<<std::endl;
+	    l.erase(l.begin(), l.begin()+1);
+	}
         inptFile.close();
     }
     else
@@ -37,5 +62,17 @@ bool Reader::parseFile(const char *fileName, int numThreads)
         std::cout<<"Failed to open file"<<std::endl;
         return false;
     }
+
+    // Join threads
+    for (int thId = 0; thId < mNumThreads; ++thId)
+    {
+        mThreads[thId].join();
+    }
     return true;
+}
+
+
+void Reader::_processLines()
+{
+    std::cout<<"Hello"<<std::endl;
 }
--- Reader.h
+++ Reader.h
@@ -1,4 +1,6 @@
-
+#include <iostream>
+#include <thread>
+#include <vector>
 
 class Reader
 {
@@ -15,10 +17,21 @@ public:
     // fileName: File to be parsed(path)
     // numThreads: Number of threads to use for parsing
     // Returns true if successful or false if not.
-    bool parseFile(const char *fileName="example.txt", int numThreads=1);
+    bool parseFile(const char *fileName="example.txt", int numThreads=4);
 
 private:
     const char *mFileName;
     int mNumThreads;
+    std::vector<std::thread> mThreads;
+
+    class Buffer
+    {
+    public:
+        Buffer() : isReady(false){}
+        bool isReady;
+        std::vector<std::string> lines;
+    };
+
+    void _processLines();    
 };
 
